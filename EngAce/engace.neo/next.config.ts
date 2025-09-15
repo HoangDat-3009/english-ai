39713866a3f5836281047fbbc3015c7ef9ec2872
HoangDat309 <datnguyenhoang5@gmail.com>
2025-09-15 12:31:29 +0700

    first commit

---
+++ EngAce/engace.neo/next.config.ts
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['placehold.co', 'i.imgur.com'], // Thêm domain ảnh này vào đây
+  },
+};
+
+module.exports = nextConfig;
